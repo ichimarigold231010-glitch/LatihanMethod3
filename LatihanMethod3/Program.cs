27a92583fb1cb1d61318a815e94b0dc2734796f5
ichimarigold231010-glitch <ichimarigold231010@gmail.com>
2026-01-28 13:28:25 +0700

    hitung l.lingkaran, persegi-panjang, segitiga

--- LatihanMethod3/Program.cs
+++ LatihanMethod3/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿int luaspersegi, luaspersegipanjang;
+double luaslingkaran, luassegitiga;
+
+
+double hitungluaslingkaran(double jari, double pi = 3.14)
+{
+    return pi * jari * jari;
+}
+
+int hitungluaspersegi(int sisi)
+{
+    return sisi * sisi;
+}
+
+int hitungluaspersegipanjang(int panjang, int lebar)
+{
+    return panjang * lebar;
+}
+
+double hitungluassegitiga(double alas, double tinggi)
+{
+    return 0.5 * alas * tinggi;
+}
+
+Console.WriteLine(hitungluaslingkaran(50));
+Console.WriteLine(hitungluaspersegi(10));
+Console.WriteLine(hitungluaspersegipanjang(20, 10));
+Console.WriteLine(hitungluassegitiga(8, 3));
